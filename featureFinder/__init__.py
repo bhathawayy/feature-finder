3a8861a7dc5eba9c75426e1c9f00c24c6654981b
Brooke Hathaway <29876155+bhathawayy@users.noreply.github.com>
2026-01-16 13:17:48 -0700

    Update __init__.py

--- featureFinder/__init__.py
+++ featureFinder/__init__.py
@@ -1,14 +1,1 @@
-import os
-import warnings
 
-
-def custom_formatwarning(message, category, filename, lineno, line=None):
-    return f"{category.__name__}: {message}\n"
-
-
-# Overwrite warnings functionality
-warnings.formatwarning = custom_formatwarning
-warnings.filterwarnings('ignore', category=RuntimeWarning)
-
-# Define max CPU cores of current machine
-MAX_CORES = min(int(os.cpu_count() / 6), 4)
